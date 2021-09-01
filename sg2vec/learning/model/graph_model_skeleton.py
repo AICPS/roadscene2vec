3f2308f66c381e443522426f7280ccffd2c41a1d
Arnav Malawade <malawada@uci.edu>
2021-08-31 17:15:37 -0700

    start building graph_model_skeleton

---
+++ sg2vec/learning/model/graph_model_skeleton.py
@@ -0,0 +1,42 @@
+import sys, os
+sys.path.append(os.path.dirname(sys.path[0]))
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchnlp.nn import Attention
+from torch.nn import Linear, LSTM
+from torch_geometric.nn import RGCNConv, TopKPooling, FastRGCNConv
+from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool
+from .rgcn_sag_pooling import RGCNSAGPooling
+
+'''Model skeleton for creating custom scene graph learning models'''
+class CustomGraphModel(nn.Module):
+    def __init__(self, config):
+        super(CustomGraphModel, self).__init__()
+        #problem specification settings
+        self.num_features = config.model_configuration['num_of_classes'] #number of node features #TODO: change num_of_classes to num_features or node_features
+        self.num_relations = config.model_configuration['num_relations'] #number of relation types.
+        self.num_classes  = config.model_configuration['nclass'] #number of possible output classes.
+        
+
+        #graph learning model definition'
+        self.num_layers = config.model_configuration['num_layers'] #defines number of graph learning layers.
+        self.hidden_dim = config.model_configuration['hidden_dim'] #hidden size of each graph learning layer.
+        self.layer_spec = None if config.model_configuration['layer_spec'] == None else list(map(int, config.model_configuration['layer_spec'].split(','))) #allows manual specification of each layer's size
+        self.rgcn_func = FastRGCNConv if config.model_configuration['conv_type'] == "FastRGCNConv" else RGCNConv #define graph convolution operation performed
+        self.activation = F.relu if config.model_configuration['activation'] == 'relu' else F.leaky_relu #define activation function used
+        self.pooling_type = config.model_configuration['pooling_type'] #define type of graph pooling layer
+        self.readout_type = config.model_configuration['readout_type'] #define type of graph readout operation
+
+        #temporal modeling settings
+        self.temporal_type = config.model_configuration['temporal_type'] #define type of temporal modeling used
+        self.lstm_dim1 = config.model_configuration['lstm_input_dim']
+        self.lstm_dim2 = config.model_configuration['lstm_output_dim']
+
+        #regularizers
+        self.dropout = config.model_configuration['dropout']
+
+        #TODO: implement model construction
+
+    def forward(self, edge_index, edge_attr, batch=None):
+        pass #TODO: implement
